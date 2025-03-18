0db42901fd8a403ad1bb154260c4754f990c235d
Paul Elliott <paul@vizworkshop.com>
2025-03-18 01:21:21 +0000

    feat: add VolView and Spin Controls projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,17 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'VolView',
+    description: `An open source medical image viewer for the web`,
+    imgSrc: '/static/images/volview.jpg',
+    href: 'https://github.com/Kitware/VolView',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'spin-controls',
+    description: `Arcball style controls for three.js Object3Ds and Cameras. 
+    Featuring pointer to trackball accuracy and unlimited rotation.`,
+    imgSrc: '/static/images/spin-controls.png',
+    href: 'https://github.com/PaulHax/spin-controls',
   },
 ]
 
